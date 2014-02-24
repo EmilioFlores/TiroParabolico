60104f22d17f30c792956f674c0812df1aaf7c84
GDanielRodriguez <danyrod94@gmail.com>
2014-02-24 11:42:27 -0600

    DELETED LINE

--- src/tiroparabolico/TiroParabolico.java
+++ src/tiroparabolico/TiroParabolico.java
@@ -18,7 +18,7 @@ public class TiroParabolico {
     public static void main(String[] args) {
         // TODO code application logic here
         //Hola esta es una prueba
-        // rudo es un caco
+     
         
     }
     
